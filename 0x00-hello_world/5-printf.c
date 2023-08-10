4c309b6d61136da9e752c274a7a8d9be419e0a9d
Joseph Conduah <josephconduah02@gmail.com>
2023-08-10 09:50:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
-/* Betty style C program that prints "with proper grammar, but the outcome is a piece of art," followed by a new line. */
-
-#include <stdio.h>
-
-int main(void) {
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
   printf("with proper grammar, but the outcome is a piece of art,\n");
   return 0;
 }
