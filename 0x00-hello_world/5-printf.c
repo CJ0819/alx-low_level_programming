b8b0a19e7d6076314c711c24530308fcfb9308e3
Joseph Conduah <josephconduah02@gmail.com>
2023-08-10 09:46:57 -0700

    work

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
+/* Betty style C program that prints "with proper grammar, but the outcome is a piece of art," followed by a new line. */
+
 #include <stdio.h>
 
-int main() {
+int main(void) {
   printf("with proper grammar, but the outcome is a piece of art,\n");
   return 0;
 }
